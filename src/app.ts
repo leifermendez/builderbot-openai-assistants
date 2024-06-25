16f21da0fe84a8d23b3ed6abe4b52340ee6d6b50
Sergio Castro <SC9Sergio@gmail.com>
2024-06-25 09:22:40 -0500

    Ya funciona con imagenes

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import { BaileysProvider as Provider } from '@builderbot/provider-baileys'
 import { toAsk, httpInject } from "@builderbot-plugins/openai-assistants"
 import { recording, typing } from "./utils/presence"
 //import { pipeline, WhisperProcessor, WhisperForConditionalGeneration } from '@xenova/transformers';
-import path, { dirname } from "path"
+import path, { dirname, join } from "path"
 import { fileURLToPath } from "url";
 import OpenAI from "openai";
 
@@ -16,21 +16,10 @@ const __dirname = dirname(__filename);
 
 const PORT = process.env?.PORT ?? 3008
 const ASSISTANT_ID = process.env?.ASSISTANT_ID ?? ''
-/*
-const pipe = await pipeline(
-    'automatic-speech-recognition'
-    , 'openai/whisper-large'
-);
-*/
+
 const welcomeFlow = addKeyword<Provider, Database>(EVENTS.WELCOME)
     .addAction(async (ctx, { flowDynamic, state, provider }) => {
         await typing(ctx, provider)
-        /*let response = await toAsk(ASSISTANT_ID, ctx.body, state)
-        console.log("original response:")
-        console.log(response)
-        response = response.replaceAll(/\[.*?\]/g, '').replaceAll(/【.*?】/g, '')
-        console.log("format response:")
-        console.log(response)*/
 
         await responseText(ctx.body, state, flowDynamic)
     })
@@ -39,17 +28,83 @@ async function responseText(text: string, state: any, flowDynamic: any) {
     const response = await responseFromAI(text, state)
     const chunks = response.split(/\n\n+/);
     for (const chunk of chunks) {
-        await flowDynamic([{ body: chunk.trim() }]);
+        await showResponseFlowDynamic(chunk, flowDynamic);
     }
 }
 
+async function showResponseFlowDynamic(chunk, flowDynamic) {
+    //Original chunk: Antonella - Tallas 27 al 33, Precio: $4,400, Color: Negro ![Antonella](attachment:3-Antonella)
+    //Format chunk:  Antonella - Tallas 27 al 33, Precio: $4,400, Color: Negro
+    const formatChunk = chunk
+        .replaceAll(/\[.*?\]/g, '')
+        .replaceAll(/【.*?】/g, '')
+        //remove ![Antonella](attachment:3-Antonella)
+        .replaceAll(/!\[.*?]\(.*?\)/g, '')
+        ;
+    //get Images
+
+    // reformat from ![Antonella](attachment:3-Antonella) to [image:3-Antonella]
+    const imagesChunk = chunk.replaceAll(/!\[.*?]\(attachment:.*?\)/g, '[image:$2]');
+    const images1 = imagesChunk.match(/\[image:(.*?)\]/g)
+
+    const images = [...(images1 || [])].filter(Boolean)
+
+    const pathFolderImages = __dirname + '/images/Inalkem'
+
+
+    console.log('Original')
+    console.log(chunk)
+    console.log('Format')
+    console.log(formatChunk)
+    console.log('Images')
+    console.log(images)
+    // if number of images is 0 then show text
+    if (images == null || images.length === 0 || images.length > 1) {
+        await flowDynamic([{ body: formatChunk.trim() }]);
+    }
+
+    if (images == null || images.length === 0) {
+        return
+    }
+
+    // if number of images is 1 then show only a flow Dynamic    
+    if (images.length === 1) {
+        console.log("Print one image")
+        const formatImage = images[0].remplaceAll('[image:', '').remplaceAll(']', '')
+            //remove ()[]
+            .remplaceAll(/\[.*?\]/g, '')
+        const pathImage = pathFolderImages + '/' + formatImage + '.webp'
+        console.log('Path Image: ' + pathImage)
+        await flowDynamic(
+            [{
+                body: formatChunk.trim(),
+                media: pathImage
+            }]);
+    }
+
+    // if number of images is more then show first a flow Dynamic with text and imagen and then show a flow Dynamic for each image
+    if (images.length > 1) {
+        console.log("Print multiple images")
+        for (const image of images) {
+            const pathImage = pathFolderImages + '/' + image.replace('[image:', '').replace(']', '') + '.webp'
+            console.log('Path Image: ' + pathImage)
+            await flowDynamic(
+                [{
+                    body: '',
+                    media: pathImage
+                }]);
+        }
+    }
+
+}
+
 async function responseFromAI(text, state) {
-    let response = await toAsk(ASSISTANT_ID, text, state)
-    console.log("original response:")
-    console.log(response)
-    response = response.replaceAll(/\[.*?\]/g, '').replaceAll(/【.*?】/g, '')
-    console.log("format response:")
-    console.log(response)
+    const response = await toAsk(ASSISTANT_ID, text, state)
+    //console.log("original response:")
+    //console.log(response)
+    //response = response.replaceAll(/\[.*?\]/g, '').replaceAll(/【.*?】/g, '')
+    //console.log("format response:")
+    //console.log(response)
     return response
 }
 
@@ -57,31 +112,17 @@ const audioFlow = addKeyword<Provider, Database>(EVENTS.VOICE_NOTE)
     .addAction(async (ctx, { flowDynamic, state, provider }) => {
         await recording(ctx, provider)
         console.log(ctx)
-        //create a local file temp.ogg
-        //const tempPath = createTempFile(__dirname, 'temp.ogg');
-        //console.log(tempPath)
 
         console.log(__dirname)
         //write the audio file
         const localPath = await provider.saveFile(ctx, { path: __dirname + "/tmp" });
         console.log(localPath)
 
-        //const audioDataBuffer = fs.readFileSync(localPath);
-
-
-        // Asegurarse de que la longitud del Buffer sea un múltiplo de 4
-        //const bufferLength = audioDataBuffer.byteLength;
-        //const adjustedLength = bufferLength - (bufferLength % 4);
-        //const adjustedBuffer = audioDataBuffer.slice(0, adjustedLength);
-
-
-        //console.log("Hasta aquí funciona")
-        //const audioData = new Float32Array(adjustedBuffer);
-        //convert the audio file to text
         const text = await speechToText(localPath);
-        console.log(text);
+        //const text2 = await speechToText(localPath);
+        //console.log(text);
         //await flowDynamic([{ body: "En esta demo no se admite audio" }]);
-        await responseText(text, state, flowDynamic)
+        await responseText("(Audio: " + text + ")", state, flowDynamic)
     })
 
 const main = async () => {
@@ -106,10 +147,9 @@ async function speechToText(filePath) {
         file: fs.createReadStream(filePath),
         model: "whisper-1",
     });
-
-    //console.log(transcription.text);
+    console.log("Transcription:");
+    console.log(transcription.text);
     return transcription.text;
 }
 
-//main2()
 main()
