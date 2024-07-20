7cd52805b60c6dd07e342e0e3dada10ac609f24f
Sergio Castro <SC9Sergio@gmail.com>
2024-07-20 10:58:11 -0500

    se cambia codigo para que no sea async

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,13 @@
 import "dotenv/config";
 
-const activeFacebook = process.env.BOT_FACEBOOK_SUPPORT;
 const activeWhatsApp = process.env.BOT_WHATSAPP_SUPPORT;
-
-async function startBots() {
-  if (activeWhatsApp === 'true') {
+if (activeWhatsApp === 'true') {
     const { startBotWhatsApp } = await import("./bots/whatsapp");
     startBotWhatsApp();
-  }
-
-  if (activeFacebook === 'true') {
-    const { startBotFacebook } = await import("./bots/facebook");
-    startBotFacebook();
-  }
 }
 
-startBots();
+const activeFacebook = process.env.BOT_FACEBOOK_SUPPORT;
+if (activeFacebook === 'true') {
+    const { startBotFacebook } = await import("./bots/facebook");
+    startBotFacebook();
+}
