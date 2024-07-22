bd3eee4d870fe17b2fb0f88caf303d308c4dd0fd
Sergio Castro <SC9Sergio@gmail.com>
2024-07-22 11:14:54 -0500

    se cambia la forma de hacer el console.log para tratar de imprimir todo la información que manda Facebook

--- src/bots/facebook.ts
+++ src/bots/facebook.ts
@@ -6,18 +6,30 @@ const app = express();
 const port = 3009;
 const validToken = process.env.FACEBOOK_VALIDATION_TOKEN
 
+function cloneObjectWithProperties(originalObject, newObject) {
+  for (const key in originalObject) {
+    // eslint-disable-next-line no-prototype-builtins
+    if (originalObject.hasOwnProperty(key)) {
+      // check if the property/key is object
+      if (typeof originalObject[key] === 'object') {
+        // call the function recursively
+        newObject[key] = {};
+        cloneObjectWithProperties(originalObject[key], newObject[key]);
+      } else {
+        newObject[key] = originalObject[key];
+      }
+    }
+  }
+}
+
 export function startBotFacebook(){
 
     app.use(express.json()); // Middleware para parsear JSON
 
     app.post('/webhook', (req, res) => {
       console.log('Webhook in post recibido:');
-      console.log(req.body.entry);
+      console.log(JSON.stringify(req, null, 2));
       console.log('Webhook verificado');
-      console.log(req.body.entry.messaging);
-      //console.log('Webhook verificado');
-      //console.log('Query:');
-      //console.log(req.query);
       
       // Verificar que el webhook proviene de Facebook
       if (req.query['hub.mode'] === 'subscribe' &&
