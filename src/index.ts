7c7380abaf2c68bf984e33fcd7987770163c9786
Sergio Castro <SC9Sergio@gmail.com>
2024-07-24 16:47:49 -0500

    se habilita bot de whatsapp

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config"
 
-//import { startBotWhatsApp } from "./bots/whatsapp";
-import { startBotFacebook } from "./bots/facebook";
+import { startBotWhatsApp } from "./bots/whatsapp";
+//import { startBotFacebook } from "./bots/facebook";
 
-//startBotWhatsApp()
-startBotFacebook();
+startBotWhatsApp()
+//startBotFacebook();
