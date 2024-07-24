700e9a6859c828e0252dcdfb605c8fa1dde10cc0
Sergio Castro <SC9Sergio@gmail.com>
2024-07-24 11:59:09 -0500

    se actualizan links de las imagenes

--- src/ImagePathList.ts
+++ src/ImagePathList.ts
@@ -1,14 +1,14 @@
-//Crear un diccionario de imagenes 
+//Crear un diccionario de imágenes 
 export const ImagePathList = {
-    '1-Antonella': "https://i.ibb.co/7VsD7w4/01-Antonella.png",
-    '2-Barbie': "https://i.ibb.co/0jW7CXh/02-Barbie.png",
-    '3-Adid': "https://i.ibb.co/Yy76txH/03-Adid.png",
-    '4-AtlantaDama': "https://i.ibb.co/fSVXC0T/04-Atlanta-Dama.png",
-    '5-AtlantaExtra': "https://i.ibb.co/HFKkxCq/05-Atlanta-Extra.png",
-    '6-Sneakers': "https://i.ibb.co/s20rz2b/06-Sneakers.png",
-    '7-Nitro': "https://i.ibb.co/44GYXmp/07-Nitro.png",
-    '8-Mia': "https://i.ibb.co/wJs7Sw2/08-Mia.png",
-    '9-Stefany': "https://i.ibb.co/9qFjx0g/09-Stefany.png",
-    '10-Brahama': "https://i.ibb.co/RN7vz9p/10-Brahama.png",
-    '11-PlantillaBrahama': "https://i.ibb.co/r6vv9x7/11-Plantilla-Brahama.png",
+    '1-Antonella': "https://i.ibb.co/vBHQZ9k/01-Antonella.png",
+    '2-Barbie': "https://i.ibb.co/PZCzxQN/02-Barbie.png",
+    '3-Adid': "https://i.ibb.co/LN2qmjy/03-Adid.png",
+    '4-AtlantaDama': "https://i.ibb.co/kghndrt/04-Atlanta-Dama.png",
+    '5-AtlantaExtra': "https://i.ibb.co/dkNQPpj/05-Atlanta-Extra.png",
+    '6-Sneakers': "https://i.ibb.co/q5HxjTT/06-Sneakers.png",
+    '7-Nitro': "https://i.ibb.co/PxFcXVd/07-Nitro.png",
+    '8-Mia': "https://i.ibb.co/TKQQjTd/08-Mia.png",
+    '9-Stefany': "https://i.ibb.co/1960Gmb/09-Stefany.png",
+    '10-Brahama': "https://i.ibb.co/7RczqV6/10-Brahama.png",
+    '11-PlantillaBrahama': "https://i.ibb.co/0VxVhQf/11-Plantilla-Brahama.png",
 };
